2ab2e58e6a3ef8b5be74f4cb31fca4506ac54a83
victormmorales <victor.morales@criptan.com>
2022-04-23 20:20:43 +0200

    crytocurrencies prices

---
+++ src/App.jsx
@@ -0,0 +1,85 @@
+import { useState, useEffect } from "react";
+import styled from "@emotion/styled";
+import Formulario from "./components/Formulario";
+import Spinner from "./components/Spinner";
+import Resultado from "./components/Resultado";
+import ImagenCripto from "./img/imagen-criptos.png";
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media (min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`;
+
+const Imagen = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`;
+
+const Heading = styled.h1`
+  font-family: "Lato", sans-serif;
+  color: #fff;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after {
+    content: "";
+    width: 100px;
+    height: 6px;
+    background-color: #66a2fe;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
+`;
+
+function App() {
+  const [monedas, setMonedas] = useState({});
+  const [resultado, setResultado] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (Object.keys(monedas).length > 0) {
+      const { moneda, criptomoneda } = monedas;
+
+      const cotizarCripto = async () => {
+        setLoading(true);
+        const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+
+        const response = await fetch(url);
+        const result = await response.json();
+        setResultado(result.DISPLAY[criptomoneda][moneda]);
+        setLoading(false);
+      };
+
+      cotizarCripto();
+    }
+  }, [monedas]);
+
+  return (
+    <Contenedor>
+      <Imagen src={ImagenCripto} alt="imagen cripto" />
+      <div>
+        <Heading>Cotizador de Criptomonedas</Heading>
+        <Formulario setMonedas={setMonedas} />
+
+        {loading ? (
+          <Spinner />
+        ) : (
+          resultado.PRICE && <Resultado resultado={resultado} />
+        )}
+      </div>
+    </Contenedor>
+  );
+}
+
+export default App;
---
+++ src/data/monedas.js
@@ -0,0 +1,8 @@
+const monedas = [
+  { id: "EUR", nombre: "Euros (€)" },
+  { id: "USD", nombre: "Dólares ($)" },
+  { id: "GBP", nombre: "Libras (£)" },
+  { id: "MXN", nombre: "Pesos ($)" },
+];
+
+export { monedas };
